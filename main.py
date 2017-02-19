2ac8dd1db1c23f334f72dbf42a68b0d5f99e4dcd
Lisa Ruby <lruby1203@hotmail.com>
2017-02-19 15:31:47 -0600

    Post form displays. Error checking completed. Starting Success case

--- main.py
+++ main.py
@@ -14,12 +14,48 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
+import webapp2, cgi, jinja2, os, re
+from google.appengine.ext import db
+from datetime import datetime
+#import hashutils
+
+# set up jinja
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class BlogEntry(db.Model):
+    title = db.StringProperty(required = True)
+    entry = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        t = jinja_env.get_template("newpost.html")
+        content = t.render()
+        self.response.write(content)
+
+    def post(self):
+        title = self.request.get("title")
+        entry = self.request.get("entry")
+        error = False
+        if title and entry:
+            self.redirect("/blog")
+        else:
+            error = True
+            t = jinja_env.get_template("newpost.html")
+            content = t.render(error = error, title=title, entry=entry)
+            self.response.write(content)
+
+class ViewBlog(MainHandler):
+    def get(self):
+        title = self.request.get("title")
+        entry = self.request.get("entry")
+        t = jinja_env.get_template("blog.html")
+        content = t.render(title=title, entry=entry)
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/blog', ViewBlog)
 ], debug=True)
